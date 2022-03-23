43de14dcb3cf69ed11544f7dff518e3291102ff1
erik1998serrano <erik1998serrano@outlook.com>
2022-03-24 01:04:06 +1100

    updated html/assets added js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// function copyText() {
+//     /* Get the text field */
+//     var copyText = document.getElementById("myEmail");
+
+//     /* Select the text field */
+//     copyText.select();
+//     copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+//     /* Copy the text inside the text field */
+//     navigator.clipboard.writeText(copyText.value);
+
+//     /* Alert the copied text */
+//     alert("Copied the text: " + copyText.value);
+// }
+
+function Hello() {
+    var copyText = document.getElementById('myInput')
+    copyText.select();
+    document.execCommand('copy')
+    console.log('Copied Text')
